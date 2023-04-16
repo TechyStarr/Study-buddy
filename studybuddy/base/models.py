2dfc7288713622efcddd487b32937cd8936f59f8
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-16 22:35:41 +0100

    create few routes

--- studybuddy/base/models.py
+++ studybuddy/base/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
-class Room(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Topic(models.Model):
     name = models.CharField(max_length=200)
-    # host = models.CharField(max_length=200)
-    # topic = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True, blank=True)
     # participants = models.ManyToManyField(User, related_name='participants')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,3 +23,14 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # one to many relationship
+    
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # cascade means if the room is deleted, delete all messages in that room
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Let's learn Python together!"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Let's learn React"},
-    {'id': 4, 'name': "Let's learn Django"},
-    {'description': "This is a room for Python learners. Feel free to ask questions and help others."}
-]
+# rooms = [
+#     {'id': 1, 'name': "Let's learn Python together!"},
+#     {'id': 2, 'name': "Design with me"},
+#     {'id': 3, 'name': "Let's learn React"},
+#     {'id': 4, 'name': "Let's learn Django"},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+def create_room(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
