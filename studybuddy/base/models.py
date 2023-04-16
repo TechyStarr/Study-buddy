69551fae5054ee7013229aa9cce893d0e90baf38
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-16 19:06:44 +0100

    created room table

--- studybuddy/base/models.py
+++ studybuddy/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    # host = models.CharField(max_length=200)
+    # topic = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = models.ManyToManyField(User, related_name='participants')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -16,9 +16,9 @@ def home(request):
 
 def room(request, pk):
     room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
 
     context = {'room': room}
 
