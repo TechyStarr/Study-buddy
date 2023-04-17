625c381103e4604d6f12b67400d35c6e5fcbed7d
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-17 17:47:15 +0100

    created login function

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,23 +1,57 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Room, Topic
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .forms import RoomForm
 
 # Create your views here.
 
-# rooms = [
-#     {'id': 1, 'name': "Let's learn Python together!"},
-#     {'id': 2, 'name': "Design with me"},
-#     {'id': 3, 'name': "Let's learn React"},
-#     {'id': 4, 'name': "Let's learn Django"},
-# ]
+def loginPage(request):
+    
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist')
+
+
+    context = {}    
+    return render(request, 'base/login_register.html', context)
+
+
+
+
+
+
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q) # filter by topic name, icontains means case insensitive
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        ) # filter by topic name, icontains means case insensitive
+
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
